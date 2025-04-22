619727c5bb2320ada771ac2e57491b1c92d843d4
Vigo28 <gamingvigo06@gmail.com>
2025-04-22 14:56:53 +0300

    .\contact-sent.php

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
+<?php
+$settings = ['Theme' => 'Dark', 'Font' => 'Arial']
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+<style>
+        body {
+            background-color: <?php echo $settings['Theme']?>;
+            font-family: <?php echo $settings['Font']; ?>;
+        }
+    </style>
 </head>
 <body>
+<ul>
     <?php
-
+foreach($settings as $key => $value){
+    echo "<li> " . $key . " = " . $value;
+}
     ?>
+</ul>
 </body>
 </html>
---
+++ sql-store.php
@@ -0,0 +1,26 @@
+
+<?php
+
+$host = "localhost";
+$username = "sumsumBoom"; 
+$password = "password"; 
+$dbname = "sql_store";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM customers";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["customer_id"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. "<br>";
+      }
+} else {
+  echo "0 results";
+}
+$conn->close();
